be53940743c20a744c030640a318579a4be72335
zzzzzzzzzzzzzzzzzzzz <cccc@bai.vhj>
2022-10-20 17:32:03 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> user = new List<int>() { 1,2,3,4};
+            user.Add(1);
+            foreach (var item in user)
+            {
+                Console.WriteLine(item);
+            }
+           
+            Console.ReadKey();
+
+            
+        }
+    }
+}
