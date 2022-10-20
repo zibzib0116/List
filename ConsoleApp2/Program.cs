6dc79be2161d7066adda339f00f0379296341a0d
zzzzzzzzzzzzzzzzzzzz <cccc@bai.vhj>
2022-10-20 17:54:54 +0400

    Revert "addRemove"
    This reverts commit a1a166edfc93de9b3b8d8c015352568d9379d581.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,12 +12,10 @@ namespace ConsoleApp2
         {
             List<int> user = new List<int>() { 1,2,3,4};
             user.Add(1);
-            user.RemoveAt(0);
             foreach (var item in user)
             {
                 Console.WriteLine(item);
             }
-
            
             Console.ReadKey();
             HashSet<int> unical = new HashSet<int>();
